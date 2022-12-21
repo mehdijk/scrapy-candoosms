3530ab5a6189850ef966b2f8d16f6d6f3d8dec90
Mehdi Jalili <jalili.m@gmail.com>
2022-12-21 11:34:20 +0330

    add page

--- eftekharazin/spiders/candoosms.py
+++ eftekharazin/spiders/candoosms.py
@@ -22,25 +22,28 @@ class CandoosmsSpider(scrapy.Spider):
     def parse_homepage(self,response):
         rand = response.css('button.offset2::attr(onclick)').extract()[0]
         rand = rand.replace('DoSearch','').replace('(this);','')
-        for i in range(7500,9746):  #9746
-            yield scrapy.Request(f'https://my.candoosms.com/ms-sendbox?useajax=true&rand={rand}&fid=&page={i}&order=outbound_message_id:DESC&filter=', 
+        for i in range(1,2):  #3613 4,170
+            request = scrapy.Request(f'https://my.candoosms.com/ms-sendbox?useajax=true&rand={rand}&fid=&page={i}&order=outbound_message_id:DESC&filter=', 
                 callback=self.extractTable,
                                     # errback=self.errback_httpbin,
                                     dont_filter=True,
                                     )
+            request.meta['p'] = i
+            yield request
     
     def extractTable(self,response):
         cells = response.css('tr td.tbl-cell')
         statusCells = response.css('tr td.tbl-cell i::attr(title)').extract()
         if len(cells)<70:
             self.logger.error(len(cells))
-        self.logger.info(len(cells))
+        self.logger.debug(f'page:{response.meta["p"]}')
         for i in range(0,len(cells)//10):
             yield {
                 'id' : cells[0+i*10].css('::text').get(default=''),
                 'sender' : cells[2+i*10].css('::text').get(default=''),
                 'construct_at' : cells[4+i*10].css('::text').get(default=''),
                 'status' : statusCells[i],
-                'crawl_at' : datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                'crawl_at' : datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                'page': response.meta['p']
             }
 
